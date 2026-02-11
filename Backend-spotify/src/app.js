e5dce31e3f0aac2edf40c109fc76c13f76793943
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-12 00:29:34 +0530

    Add files via upload

---
+++ Backend-spotify/src/app.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const cookieParser=require('cookie-parser')
+const authRoutes=require('./routes/auth.routes')
+const musicRoutes=require('./routes/music.routes')
+
+const cors=require("cors");
+
+app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+  origin: true,
+  credentials: true
+}));
+
+
+const app=express()
+app.use(express.json())
+app.use(cookieParser())
+app.use('/api/auth',authRoutes)
+app.use('/api/music',musicRoutes)
+
+module.exports=app
---
+++ Backend-spotify/src/middlewares/auth.middleware.js
@@ -0,0 +1,51 @@
+const jwt=require('jsonwebtoken')
+
+async function authArtist(req,res,next){
+    const token=req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:'Unauthorized access by user'
+        })
+    }
+    try {
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        if(decoded.role!=="artist"){
+            return res.status(403).json({
+                message:'Only artists can access.'
+            })
+        }
+        req.user=decoded
+        next()
+    }
+    catch(err){
+        return res.status(401).json({
+            message:'Unauthorised access.'
+        })
+    }
+}
+
+async function authUser(req,res,next){
+    const token=req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:'Unauthorized access by user'
+        })
+    }
+    try {
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+        if(decoded.role!=="user"){
+            return res.status(403).json({
+                message:'Only users can access.'
+            })
+        }
+        req.user=decoded
+        next()
+    }
+    catch(err){
+        return res.status(401).json({
+            message:'Unauthorised access.'
+        })
+    }
+}
+
+module.exports={authArtist,authUser}
