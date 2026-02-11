03c08a5238b9d53d72f2e740ecd602d40cffd19b
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-12 00:27:52 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]); //->vvi to add in server.js worked successfully.
-const app=require('./src/app')
-const connectDB = require('./src/db/db')
-connectDB()
-app.listen(3000,()=>{
-    console.log("Hii, server is running on port: 3000.")
-})
