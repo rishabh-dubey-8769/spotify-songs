de4ffd4c4e566cd8d82c345d56cf2deae753afdd
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-13 23:28:10 +0530

    Update music.controller.js

--- Backend-spotify/src/controllers/music.controller.js
+++ Backend-spotify/src/controllers/music.controller.js
@@ -5,7 +5,17 @@ const jwt=require('jsonwebtoken')
 
 async function createMusic(req,res){
     const {title}=req.body
+    if(!title){
+      return res.status(400).json({
+        message:"music title is required"
+      });
+    }
     const file=req.file
+    if(!file){
+      return res.status(400).json({
+        message:"Music file is required"
+      });
+    }
     const result =await uploadFile(file.buffer.toString('base64'))
     const music=await musicModel.create({
         uri:result.url,
@@ -26,13 +36,20 @@ async function createMusic(req,res){
 }
 
 async function createAlbum(req,res){
+  try{
+    const {title,musics}=req.body;
 
-    const {title,musics}=req.body
+    if(!title){
+      return res.status(400).json({
+        message:"Album title is required"
+      });
+    }
+
     const album=await albumModel.create({
         title,
-        musics:musics,
+        musics:musics||[],
         artist:req.user.id
-    })
+    });
 
     res.status(201).json({
         message:'Album created successfully',
@@ -42,15 +59,19 @@ async function createAlbum(req,res){
             musics:album.musics,
             artist:album.artist
         }
-    })
+    });
 
-}    
+  }catch(err){
+    res.status(500).json({message:"Server error"});
+  }
+}
+ 
 
 async function getAllMusics(req,res){
     const musics=await musicModel
     .find()
-    .skip(1)
-    .limit(2)
+    .skip(0)
+    .limit(req.query.limit || 6)
     .populate('artist','username email')
 
     res.status(200).json({
@@ -60,7 +81,10 @@ async function getAllMusics(req,res){
 }
 
 async function getAllAlbums(req,res){
-    const albums=await albumModel.find().select("title artist").populate('artist','username email')
+    const albums=await albumModel
+        .find({ artist: req.user.id })
+        .select("title artist")
+        .populate('artist','username email')
     res.status(200).json({
         message:'Albums fetched successfully',
         albums:albums
@@ -69,7 +93,10 @@ async function getAllAlbums(req,res){
 
 async function getAlbumById(req,res){
     const albumId=req.params.albumId
-    const album=await albumModel.findById(albumId).populate('musics').populate('artist','username email')
+    const album=await albumModel
+        .findById(albumId)
+        .populate('musics')
+        .populate('artist','username email')
     if(!album){
         return res.status(404).json({
             message:'Album not found'
@@ -80,5 +107,6 @@ async function getAlbumById(req,res){
         album:album
     })
 }
+
 
 module.exports={createMusic,createAlbum,getAllMusics,getAllAlbums,getAlbumById}
