b440d8058e736602f5825a1bab9ee3bb13210dde
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-15 02:03:13 +0530

    Update auth.routes.js

--- Backend-spotify/src/routes/auth.routes.js
+++ Backend-spotify/src/routes/auth.routes.js
@@ -7,8 +7,8 @@ router.post('/register',authController.registerUser)
 router.post('/login',authController.loginUser)
 router.post('/logout',authController.logoutUser)
 router.get('/count', authController.getUserCount);
-router.get("/me", authUserOrArtist, getMe);
-
+router.get('/me', authController.getMe);
 
 module.exports=router
+
 
