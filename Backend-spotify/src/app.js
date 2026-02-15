2ff7bfa65ac450bd506e92fa68520c0bb9247126
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-15 16:26:58 +0530

    Update app.js

--- Backend-spotify/src/app.js
+++ Backend-spotify/src/app.js
@@ -6,14 +6,27 @@ const cors=require("cors");
 
 const app=express()
 
+// app.use(cors({
+//   origin: [
+//     "http://127.0.0.1:5500",
+//     "http://localhost:5500",
+//     "https://spotify-songs-frontend.onrender.com"
+//   ],
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "https://spotify-songs-frontend.onrender.com"
-  ],
+  origin: process.env.FRONTEND_URL, // from .env
   credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 
 
 
@@ -23,5 +36,6 @@ app.use('/api/auth',authRoutes)
 app.use('/api/music',musicRoutes)
 
 module.exports=app
+
 
 
