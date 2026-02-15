930f8d049f1f40c14f3e69b68ecda8e668013a62
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-15 12:43:44 +0530

    Add OTP routes for registration and password reset
    Added routes for OTP registration and password reset.

--- Backend-spotify/src/routes/auth.routes.js
+++ Backend-spotify/src/routes/auth.routes.js
@@ -8,7 +8,14 @@ router.post('/login',authController.loginUser)
 router.post('/logout',authController.logoutUser)
 router.get('/count', authController.getUserCount);
 router.get('/me', authController.getMe);
+router.post("/send-otp-register",authController.sendOtpRegister);
+router.post("/verify-otp-register",authController.verifyOtpRegister);
+
+router.post("/send-otp-forgot",authController.sendOtpForgot);
+router.post("/reset-password",authController.resetPassword);
+
 
 module.exports=router
+
 
 
